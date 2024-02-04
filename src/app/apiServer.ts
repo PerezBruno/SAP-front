cbb2ea02e0a5aa112c3d0262845d5e562d5ba788
PerezBruno <brunoleandroperez@hotmail.com>
2024-02-04 12:23:25 -0300

    title

--- src/app/apiServer.ts
+++ src/app/apiServer.ts
@@ -1,6 +1,6 @@
 export const apiServer = {
-    //serverUrl: "https://four7285-perezbruno.onrender.com",
-    serverUrl: "http://localhost:8080",
+    serverUrl: "https://four7285-perezbruno.onrender.com",
+    //serverUrl: "http://localhost:8080",
     siteName: "BACKEND ecommerce"
 }
 
